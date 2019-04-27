4a7fca8550088907c08f5b45480484015ed73694
jeff <sharpkingdom@gmail.com>
2019-04-27 19:51:10 +0800

    Add pprof.

--- example.go
+++ example.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	app := iris.Default()
     app.Post("/api/json-format", service.FormatJson)
-    app.Post("/api/md5", service.Md5)
+	app.Post("/api/md5", service.Md5)
+	
+	// add pprof
+	app.Any("/debug/pprof/{action:path}", pprof.New())
 
 	// listen and serve on http://0.0.0.0:8080.
 	app.Run(iris.Addr("127.0.0.1:8080"))
